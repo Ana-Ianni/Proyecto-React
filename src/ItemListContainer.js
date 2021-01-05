4f30e7f5f48e5c0043fa757d8661d249369c4081
Ana Ianni <70654287+Ana-Ianni@users.noreply.github.com>
2021-01-05 12:17:27 -0300

    Add files via upload

--- src/ItemListContainer.js
+++ src/ItemListContainer.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react'
 import './style.css';
+import ItemCount from "./ItemCount";
 
 const ItemListContainer = ({greeting}) =>{
+
+    const [state, setState] = useState(0)
+    
+    const onAdd = () => {
+        setState(state + 1)
+    }
+
+    const onSubstract = () => {
+        setState(state - 1)
+    }
+
+    const reset = () => {
+        setState(0)
+    }
+    
     return (
     <>
-    <div className="itemListCont-wrap">
+    <main className="itemListCont-wrap">
         <p className="greeting-title">{greeting}</p>
-    </div>
+    </main>
+    <section className="contador-wrap">
+
+        <ItemCount 
+            state={state} 
+            onSubstract={onSubstract}
+            onAdd={onAdd}
+            reset={reset}
+        />
+
+    </section>
     </>
     );
 }
